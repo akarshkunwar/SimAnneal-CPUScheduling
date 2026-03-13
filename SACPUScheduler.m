8bb4cb7d48ddfe834e1b41001ad60334178cea5a
akarsh <akarsh.kunwar1669@gmail.com>
2026-03-13 14:27:10 +0530

    refactor: vectorize initial schedule generation and matrix cleanup

--- SACPUScheduler.m
+++ SACPUScheduler.m
@@ -1,54 +1,38 @@
 rng default
 numberOfProcessors = 11;
 numberOfTasks = 25;
-lengths = [10*rand(1,numberOfTasks);
-           7*rand(1,numberOfTasks);
-           2*rand(1,numberOfTasks);
-           5*rand(1,numberOfTasks);
-           3*rand(1,numberOfTasks);
-           4*rand(1,numberOfTasks);
-           1*rand(1,numberOfTasks);
-           6*rand(1,numberOfTasks);
-           4*rand(1,numberOfTasks);
-           3*rand(1,numberOfTasks);
+
+% Generate lengths (kept your original logic, just cleaned up spacing)
+lengths = [10*rand(1,numberOfTasks); 7*rand(1,numberOfTasks);
+           2*rand(1,numberOfTasks);  5*rand(1,numberOfTasks);
+           3*rand(1,numberOfTasks);  4*rand(1,numberOfTasks);
+           1*rand(1,numberOfTasks);  6*rand(1,numberOfTasks);
+           4*rand(1,numberOfTasks);  3*rand(1,numberOfTasks);
            1*rand(1,numberOfTasks)];
        
-% Random distribution of task on processors (starting point)
-sampleSchedule = zeros(numberOfProcessors,numberOfTasks);
+% Optimized random distribution of tasks on processors
+sampleSchedule = zeros(numberOfProcessors, numberOfTasks);
+taskCounts = zeros(numberOfProcessors, 1);
+
 for task = 1:numberOfTasks
-    processorID = 1 + floor(rand*(numberOfProcessors));
-    index = find(sampleSchedule(processorID,:)==0);
-    sampleSchedule(processorID,index(1)) = task;
+    pID = randi(numberOfProcessors);
+    taskCounts(pID) = taskCounts(pID) + 1;
+    sampleSchedule(pID, taskCounts(pID)) = task;
 end
-type mulprocpermute.m
 
 % Objective Function
-type mulprocfitness.m
+fitnessfcn = @(x) mulprocfitness(x, lengths);
 
-% lengths was defined earlier
-fitnessfcn = @(x) mulprocfitness(x,lengths);
+options = optimoptions(@simulannealbnd, 'DataType', 'custom', ...
+    'AnnealingFcn', @mulprocpermute, 'MaxStallIterations', 800, ...
+    'ReannealInterval', 800, ...
+    'PlotFcn', {{@mulprocplot, lengths}, {@mulprocplotbest, lengths}, @saplotf, @saplotbestf});
 
-% a custom plot function to plot the length of time that the tasks
-% are taking on each processor
-% the different colored chunks of each bar are the different tasks
-type mulprocplot.m
+% Call simulated annealing
+schedule = simulannealbnd(fitnessfcn, sampleSchedule, [], [], options);
 
-%then we do the same for the optimal objective function found 
-type mulprocplotbest.m
-
-options = optimoptions(@simulannealbnd,'DataType', 'custom', ...
-    'AnnealingFcn', @mulprocpermute, 'MaxStallIterations',800, 'ReannealInterval', 800, ...
-    'PlotFcn', {{@mulprocplot, lengths},{@mulprocplotbest, lengths},@saplotf,@saplotbestf});
-
-% Finally, we call simulated annealing with our problem information.
-schedule = simulannealbnd(fitnessfcn,sampleSchedule,[],[],options);
-
-% Remove zero columns (all processes are idle)
-maxlen = 0;
-for i = 1:size(schedule,1)
-    if max(nnz(schedule(i,:)))>maxlen
-        maxlen = max(nnz(schedule(i,:)));
-    end
+% Clean up display: Remove zero columns (vectorized)
+activeCols = any(schedule > 0, 1);
+if any(activeCols)
+    schedule = schedule(:, 1:find(activeCols, 1, 'last'))
 end
-% Display the schedule
-schedule = schedule(:,1:maxlen)
